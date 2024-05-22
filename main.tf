9b452cec3fe8845b8b5770e926b125a7aeefd6db
Tejaswinikilaru <tejaswinikilaru5@gmail.com>
2024-05-22 15:54:10 +0530

    adding content to files

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "EC2-1" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "EC2-1"
+  }
+}
+
+  resource "aws_instance" "EC2-2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "EC2-1"
+  }
+
+  key_name = var.key_name
+}
